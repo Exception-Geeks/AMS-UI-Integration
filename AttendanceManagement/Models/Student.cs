f0b865d6c9df9e6df2ab2a289b9ac0cebea6c4fd
sankalp7654 <sankalp.saxena.sta@gmail.com>
2018-09-10 21:59:54 +0530

    integrated manage attendance

--- AttendanceManagement/Models/Student.cs
+++ AttendanceManagement/Models/Student.cs
@@ -11,7 +11,7 @@ namespace AttendanceManagement.Models
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Student
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,14 +20,18 @@ namespace AttendanceManagement.Models
             this.Attendances = new HashSet<Attendance>();
             this.Teacher_Teaches_Student = new HashSet<Teacher_Teaches_Student>();
         }
-    
+
         public string USN { get; set; }
         public string Name { get; set; }
         public string Section { get; set; }
         public int Sem { get; set; }
         public string Department_DID { get; set; }
         public string Subject_SubCode { get; set; }
-    
+        public DateTime Date { get; set; }
+        public string Slot { get; set; }
+        public bool IsPresent { get; set; }
+
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Attendance> Attendances { get; set; }
         public virtual Department Department { get; set; }
